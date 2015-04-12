1de4c66b75c19c07273490679655ca13834efed8
Jean-Louis MOINY <jlmoiny@outlook.be>
2015-04-12 11:53:06 +0200

    Création de l'interface IPersistenceMngr Le PersistenceMngr doit l'implémenter => Doit être complété dès que le web service implémente les méthodes qui sont nécessaires à PersistenceMng

---
+++ src/java/model/IPersistenceMngr.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ *
+ * @author Jean-Louis
+ */
+
+/**
+ * TO BE IMPLEMENTED WHEN Web Service wil be completed
+ * @author Jean-Louis
+ */
+public class IPersistenceMngr {
+    
+}
