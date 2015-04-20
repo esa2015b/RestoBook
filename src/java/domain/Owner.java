fe7050dc336db92573a7ef18dbbefa78d4e118d9
Jean-Louis MOINY <jlmoiny@outlook.be>
2015-04-20 21:35:36 +0200

    Domain entity Owner can be used as JavaBean

--- src/java/domain/Owner.java
+++ src/java/domain/Owner.java
@@ -7,8 +7,47 @@ package domain;
 
 /**
  *
- * @author Thibaut
+ * @author Jean-Louis
  */
 public class Owner {
     
+    private int Id;
+    private String FirstName;
+    private String LastName;
+    private boolean IsEnable;
+    
+    public Owner(int Id, String FirstName, String LastName, boolean IsEnable){
+        this.Id=Id;
+        this.FirstName=FirstName;
+        this.LastName=LastName;
+        this.IsEnable=IsEnable;
+    }
+    
+    public Owner(){
+        
+    }
+    
+    public int getId(){
+        return this.Id;
+    }
+    
+    public void setId(int Id){
+        this.Id=Id;
+    }
+    
+    public String getFirstName(){
+        return this.FirstName;
+    }
+    
+    public void setFirstName(){
+        this.FirstName=FirstName;
+    }
+    
+    public boolean getIsEnable(){
+        return this.IsEnable;
+    }
+    
+    public void setIsEnable(){
+        this.IsEnable=IsEnable;
+    }   
 }
